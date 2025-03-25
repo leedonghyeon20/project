29e9b3bfdedced6e26b8591bd550aa7ceca06b84
yong <lye98055@gmail.com>
2025-03-25 11:25:17 +0900

    수정1

--- src/test/test1.java
+++ src/test/test1.java
@@ -3,6 +3,9 @@ package test;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("안녕하세요");
+        System.out.println("Hello World");
+        System.out.println("안녕");
+        System.out.println("안녕");
+        System.out.println("안녕");
     }
 }
