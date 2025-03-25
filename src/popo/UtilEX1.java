a7d44c61c5473cd7619ab69f70aa8b4da59175f2
yong <lye98055@gmail.com>
2025-03-25 11:19:09 +0900

    UtilEX1추가

---
+++ src/popo/UtilEX1.java
@@ -0,0 +1,11 @@
+package popo;
+
+public class UtilEX1 {
+    public static void main(String[] args) {
+
+        
+        int t = (int) Math.random() * 6;
+        System.out.println(t);
+
+    }
+}
